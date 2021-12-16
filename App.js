e93951359f28459a014e3d8edd4b1603a4981736
bgg274 <bgg274@myy.haaga-helia.fi>
2021-12-16 15:29:35 +0200

    Push to github

---
+++ App.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
+import ToDoList from "./screens/ToDoList";
+import EditList from "./screens/EditList";
+import Login from "./screens/Login";
+import Settings from "./screens/Settings";
+import Colors from "./constants/Colors";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const Stack = createStackNavigator();
+const AuthStack = createStackNavigator();
+
+const AuthScreens = () => {
+    return (
+        <AuthStack.Navigator>
+            <AuthStack.Screen name="Login" component={Login} />
+        </AuthStack.Navigator>
+    );
+};
+const Screens = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Fire App" component={Home} />
+            <Stack.Screen name="Settings" component={Settings} />
+            <Stack.Screen
+                name="ToDoList"
+                component={ToDoList}
+                options={({ route }) => {
+                    return {
+                        title: route.params.title,
+                        headerStyle: {
+                            backgroundColor: route.params.color,
+                        },
+                        headerTintColor: "white",
+                    };
+                }}
+            />
+            <Stack.Screen
+                name="Edit"
+                component={EditList}
+                options={({ route }) => {
+                    return {
+                        title: route.params.title
+                            ? `Edit ${route.params.title} list`
+                            : "Create new list",
+                        headerStyle: {
+                            backgroundColor: route.params.color || Colors.blue,
+                        },
+                        headerTintColor: "white",
+                    };
+                }}
+            />
+        </Stack.Navigator>
+    );
+};
+export default function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    useEffect(() => {
+        if (firebase.auth().currentUser) {
+            setIsAuthenticated(true);
+        }
+        firebase.auth().onAuthStateChanged((user) => {
+            console.log("Checking auth state...");
+            if (user) {
+                setIsAuthenticated(true);
+            } else {
+                setIsAuthenticated(false);
+            }
+        });
+    }, []);
+
+    return (
+        <NavigationContainer>
+            {isAuthenticated ? <Screens /> : <AuthScreens />}
+        </NavigationContainer>
+    );
+}
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDJywQJNpX7UUNVc8AeceICZ94yhaoxX_I",
+    authDomain: "todo-app-5bd95.firebaseapp.com",
+    projectId: "todo-app-5bd95",
+    storageBucket: "todo-app-5bd95.appspot.com",
+    messagingSenderId: "60611145951",
+    appId: "1:60611145951:web:87b3cbf8c6b7aa1cb7796a",
+    measurementId: "G-0HNHQPCKRS"
+  };
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+} else {
+    firebase.app();
+}
